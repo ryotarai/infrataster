1442329640ecd0a65d0d03097ee9482ffb0136b3
Ryota Arai <ryota.arai@gmail.com>
2014-04-27 20:51:25 +0900

    Use faraday.

--- infrataster.gemspec
+++ infrataster.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency "capybara"
   spec.add_runtime_dependency "selenium-webdriver"
   spec.add_runtime_dependency "browsermob-proxy"
+  spec.add_runtime_dependency "faraday"
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
--- lib/infrataster/contexts/http_context.rb
+++ lib/infrataster/contexts/http_context.rb
@@ -1,15 +1,21 @@
-require 'net/http'
-require 'net/https'
+require 'faraday'
 
 module Infrataster
   module Contexts
     class HttpContext < BaseContext
       def response
-        req = Net::HTTP::Get.new('/', {'Host' => resource.uri.host})
-
         server.from_gateway(resource.uri.port) do |address, port|
-          Net::HTTP.start(address, port) do |http|
-            http.request(req)
+          url = "#{resource.uri.scheme}://#{address}:#{port}"
+
+          conn = Faraday.new(:url => url) do |faraday|
+            faraday.request  :url_encoded
+            faraday.response :logger, Logger
+            faraday.adapter  Faraday.default_adapter
+          end
+
+          conn.get do |req|
+            req.headers['Host'] = resource.uri.host
+            req.url resource.uri.path
           end
         end
       end
