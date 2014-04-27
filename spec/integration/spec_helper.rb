76c284d6446f2a61170d638be4edc768f0966193
Ryota Arai <ryota.arai@gmail.com>
2014-04-27 16:52:52 +0900

    Update spec/integration/spec_helper.rb.

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -2,24 +2,17 @@ require 'infrataster/rspec'
 
 Infrataster::Server.define(
   :proxy,
-  '192.168.33.10',
+  '192.168.44.10',
   vagrant: true,
 )
 Infrataster::Server.define(
   :app,
-  '172.16.33.11',
+  '172.16.44.11',
   vagrant: true,
   from: :proxy
 )
-Infrataster::Server.define(
-  :db,
-  '172.16.33.12',
-  vagrant: true,
-  from: :app,
-  mysql: {user: 'app', password: 'app'}
-)
 
-Infrataster::BrowsermobProxy.bin_path = File.expand_path('../../../vendor/browsermob-proxy/bin/browsermob-proxy', __FILE__)
+Infrataster::BrowsermobProxy.bin_path = File.expand_path('../vm/vendor/browsermob/bin/browsermob-proxy', __FILE__)
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
