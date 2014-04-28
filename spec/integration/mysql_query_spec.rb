1dc70e06385c0336b41bfd7bf07e621bd726a048
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 00:01:35 +0900

    Update README about resources.

--- spec/integration/mysql_query_spec.rb
+++ spec/integration/mysql_query_spec.rb
@@ -3,6 +3,7 @@ require 'integration/spec_helper'
 describe server(:app) do
   describe mysql_query('SHOW STATUS') do
     it 'responds uptime' do
+      p results.class
       row = results.find {|r| r['Variable_name'] == 'Uptime' }
       expect(row['Value'].to_i).to be > 0
     end
