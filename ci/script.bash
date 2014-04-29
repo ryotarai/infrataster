4f065db5554cf2fe9e21fd675d7356c119215d88
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 23:37:06 +0900

    update

--- ci/script.bash
+++ ci/script.bash
@@ -1,9 +1,9 @@
 DROPLET=wercker-infrataster-$(date +%s)
-tugboat create $DROPLET
+tugboat create --size=66 --image=3101045 --region=6 $DROPLET
 tugboat wait $DROPLET
 IPADDRESS=$(tugboat info $DROPLET | grep IP: | ruby -p -e 'sub(/.+?([\d\.]+)/, "\\1")')
 sleep 10
-scp -o 'StrictHostKeyChecking no' ./ci/test.bash root@$IPADDRESS:~
-ssh -o 'StrictHostKeyChecking no' root@$IPADDRESS 'bash -x ./test.bash'
+scp -o 'StrictHostKeyChecking no' -r . root@$IPADDRESS:~/src
+ssh -o 'StrictHostKeyChecking no' root@$IPADDRESS 'cd src && bash -x ./ci/test.bash'
 tugboat destroy -c $DROPLET
 
--- ci/test.bash
+++ ci/test.bash
@@ -1,9 +1,20 @@
+set -e
+
 echo 'deb http://download.virtualbox.org/virtualbox/debian precise contrib' > /etc/apt/sources.list.d/virtualbox.list
 apt-get update
 apt-get install -y virtualbox
 
-wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.4_x86_64.deb
-dpkg -i vagrant_1.5.4_x86_64.deb
+wget -O /tmp/vagrant.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.4_x86_64.deb
+dpkg -i /tmp/vagrant.deb
 
-vagrant -v
+apt-get install -y python-software-properties
+apt-add-repository ppa:brightbox/ruby-ng
+apt-get update
+apt-get install -y ruby2.1
+
+bundle install
+bundle exec rake spec:unit
+bundle exec rake spec:integration:prepare
+bundle exec rake spec:integration
+
 
