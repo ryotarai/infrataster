20aca62bc7a0fe2106f1fa0f478b3b654a887116
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 22:54:04 +0900

    Update script.

--- ci/script.bash
+++ ci/script.bash
@@ -2,9 +2,8 @@ DROPLET=wercker-infrataster-$(date +%s)
 tugboat create $DROPLET
 tugboat wait $DROPLET
 IPADDRESS=$(tugboat info $DROPLET | grep IP: | ruby -p -e 'sub(/.+?([\d\.]+)/, "\\1")')
-echo $IPADDRESS
-sleep 30
-scp ./ci/test.bash root@$IPADDRESS
-ssh root@$IPADDRESS 'bash -x ./test.bash'
+sleep 10
+scp -c 'StrictHostKeyChecking no' ./ci/test.bash root@$IPADDRESS
+ssh -c 'StrictHostKeyChecking no' root@$IPADDRESS 'bash -x ./test.bash'
 tugboat destroy -c $DROPLET
 
