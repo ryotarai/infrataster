7abccc9d1b163dab0267df6f9d5fb1073543a95e
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 21:47:33 +0900

    Update wercker.

--- ci/script.bash
+++ ci/script.bash
@@ -2,3 +2,7 @@ tugboat create wercker-infrataster
 tugboat wait wercker-infrataster
 IPADDRESS=$(tugboat info wercker-infrataster | grep IP: | ruby -p -e 'sub(/.+?([\d\.]+)/, "\\1")')
 echo $IPADDRESS
+scp ./ci/test.bash root@$IPADDRESS
+ssh root@$IPADDRESS 'bash -x ./test.bash'
+tugboat destroy wercker-infrataster
+
