f537a72b6a47b8753aa5f4491058c485c6f3239e
Ryota Arai <ryota.arai@gmail.com>
2014-04-30 00:14:15 +0900

    update

--- ci/script.bash
+++ ci/script.bash
@@ -2,8 +2,12 @@ DROPLET=wercker-infrataster-$(date +%s)
 tugboat create --size=66 --image=3346689 --region=6 $DROPLET
 tugboat wait $DROPLET
 IPADDRESS=$(tugboat info $DROPLET | grep IP: | ruby -p -e 'sub(/.+?([\d\.]+)/, "\\1")')
-sleep 10
-rsync -a --exclude=.git -e "ssh -o 'StrictHostKeyChecking no'" . root@$IPADDRESS:~/src
+
+while true; do
+    timeout 1 ping -c1 $IPADDRESS && break
+done
+
+rsync -a -e "ssh -o 'StrictHostKeyChecking no'" . root@$IPADDRESS:~/src
 ssh -o 'StrictHostKeyChecking no' root@$IPADDRESS 'cd src && bash -x ./ci/test.bash'
 tugboat destroy -c $DROPLET
 
