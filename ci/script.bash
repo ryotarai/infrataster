2e2c8b12cc04b2384f0e38865c984aff477a5343
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 23:46:59 +0900

    update

--- ci/script.bash
+++ ci/script.bash
@@ -1,9 +1,9 @@
 DROPLET=wercker-infrataster-$(date +%s)
-tugboat create --size=66 --image=3101045 --region=6 $DROPLET
+tugboat create --size=66 --image=3346689 --region=6 $DROPLET
 tugboat wait $DROPLET
 IPADDRESS=$(tugboat info $DROPLET | grep IP: | ruby -p -e 'sub(/.+?([\d\.]+)/, "\\1")')
 sleep 10
-scp -o 'StrictHostKeyChecking no' -r . root@$IPADDRESS:~/src
+rsync -a --exclude=.git -e "ssh -o 'StrictHostKeyChecking no'" . root@$IPADDRESS:~/src
 ssh -o 'StrictHostKeyChecking no' root@$IPADDRESS 'cd src && bash -x ./ci/test.bash'
 tugboat destroy -c $DROPLET
 
--- ci/test.bash
+++ ci/test.bash
@@ -1,17 +1,5 @@
 set -e
 
-echo 'deb http://download.virtualbox.org/virtualbox/debian precise contrib' > /etc/apt/sources.list.d/virtualbox.list
-apt-get update
-apt-get install -y virtualbox
-
-wget -O /tmp/vagrant.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.4_x86_64.deb
-dpkg -i /tmp/vagrant.deb
-
-apt-get install -y python-software-properties
-apt-add-repository ppa:brightbox/ruby-ng
-apt-get update
-apt-get install -y ruby2.1
-
 bundle install
 bundle exec rake spec:unit
 bundle exec rake spec:integration:prepare
