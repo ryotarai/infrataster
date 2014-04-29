682f126b3733263e5f3832e9071e275a76654f28
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 22:57:53 +0900

    Update.

--- ci/script.bash
+++ ci/script.bash
@@ -3,7 +3,7 @@ tugboat create $DROPLET
 tugboat wait $DROPLET
 IPADDRESS=$(tugboat info $DROPLET | grep IP: | ruby -p -e 'sub(/.+?([\d\.]+)/, "\\1")')
 sleep 10
-scp -c 'StrictHostKeyChecking no' ./ci/test.bash root@$IPADDRESS
-ssh -c 'StrictHostKeyChecking no' root@$IPADDRESS 'bash -x ./test.bash'
+scp -o 'StrictHostKeyChecking no' ./ci/test.bash root@$IPADDRESS
+ssh -o 'StrictHostKeyChecking no' root@$IPADDRESS 'bash -x ./test.bash'
 tugboat destroy -c $DROPLET
 
