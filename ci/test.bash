49678d04a93fbb49621bcc6f9cb0caa550290e64
Ryota Arai <ryota.arai@gmail.com>
2014-04-29 23:03:00 +0900

    update

--- ci/test.bash
+++ ci/test.bash
@@ -1,1 +1,9 @@
-echo Hello
+echo 'deb http://download.virtualbox.org/virtualbox/debian precise contrib' > /etc/apt/sources.list.d/virtualbox.list
+apt-get update
+apt-get install -y virtualbox
+
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.4_x86_64.deb
+dpkg -i vagrant_1.5.4_x86_64.deb
+
+vagrant -v
+
