2acc80f13f2c70d497ee14daf1c1cf6f07df0485
Ryota Arai <ryota.arai@gmail.com>
2014-04-30 21:22:17 +0900

    update

--- ci/test.bash
+++ ci/test.bash
@@ -1,8 +1,8 @@
 set -e
 
-apt-get install -y git ruby2.1-dev libxslt1-dev libxml2-dev build-essential
+apt-get install -y git ruby2.1-dev libxslt1-dev libxml2-dev build-essential libgecode-dev mysqlclient-dev
 
-bundle install
+USE_SYSTEM_GECODE=1 bundle install
 bundle exec rake spec:unit
 bundle exec rake spec:integration:prepare
 bundle exec rake spec:integration
