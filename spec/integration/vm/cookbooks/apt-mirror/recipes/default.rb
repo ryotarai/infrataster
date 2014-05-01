60445d73da17af9d9d9de252c0359d063f88becd
Ryota Arai <ryota.arai@gmail.com>
2014-05-01 19:29:03 +0900

    update

--- spec/integration/vm/cookbooks/apt-mirror/recipes/default.rb
+++ spec/integration/vm/cookbooks/apt-mirror/recipes/default.rb
@@ -1,4 +1,8 @@
-execute "sed -i -e 's| \\(http[^ ]\\+\\)| mirror://mirrors.ubuntu.com/mirrors.txt|g' /etc/apt/sources.list"
+if node['ci']
+  execute "sed -i -e 's| http://[^/]\\+| http://mirrors.digitalocean.com|g' /etc/apt/sources.list"
+else
+  execute "sed -i -e 's| \\(http[^ ]\\+\\)| mirror://mirrors.ubuntu.com/mirrors.txt|g' /etc/apt/sources.list"
+end
 
 include_recipe 'apt'
 
