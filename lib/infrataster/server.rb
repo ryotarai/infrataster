35e54e47d06ad77b4b6da5b5f94281d310e6e572
Ryota Arai <ryota.arai@gmail.com>
2014-05-06 17:05:58 +0900

    Refactor Server#_ssh_start_args.

--- lib/infrataster/server.rb
+++ lib/infrataster/server.rb
@@ -114,28 +114,36 @@ module Infrataster
 
     def _ssh_start_args
       config = {}
+
       if @options[:ssh]
         config = @options[:ssh]
       elsif @options[:vagrant]
-        if @options[:vagrant] != true
-          vagrant_name = @options[:vagrant]
-        else
-          vagrant_name = @name
-        end
-
-        Dir.mktmpdir do |dir|
-          output = File.join(dir, 'ssh-config')
-          `/usr/bin/vagrant ssh-config #{Shellwords.shellescape(vagrant_name)} > #{Shellwords.shellescape(output)}`
-          if $?.exitstatus != 0
-            raise Error, "`vagrant ssh-config` failed. Please check if VMs are running or not."
-          end
-          config = Net::SSH::Config.for(@name.to_s, [output])
-        end
+        vagrant_name = if @options[:vagrant] != true
+                         @options[:vagrant]
+                       else
+                         @name
+                       end
+        config = ssh_config_for_vagrant(vagrant_name)
       else
         raise Error, "Can't get configuration to connect to the server via SSH. Please set ssh option or vagrant option."
       end
 
       [config[:host], config[:user], config]
+    end
+
+    def ssh_config_for_vagrant(name)
+      config = nil
+
+      Dir.mktmpdir do |dir|
+        output = File.join(dir, 'ssh-config')
+        `/usr/bin/vagrant ssh-config #{Shellwords.shellescape(name)} > #{Shellwords.shellescape(output)}`
+        if $?.exitstatus != 0
+          raise Error, "`vagrant ssh-config` failed. Please check if VMs are running or not."
+        end
+        config = Net::SSH::Config.for(@name.to_s, [output])
+      end
+
+      config
     end
   end
 end
