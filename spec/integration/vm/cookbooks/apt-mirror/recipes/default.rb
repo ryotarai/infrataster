d73795f7993f8f551870004177fe60e1ac88adc6
Ryota Arai <ryota.arai@gmail.com>
2014-05-10 17:39:46 +0900

    Select apt server randomly.

--- spec/integration/vm/cookbooks/apt-mirror/recipes/default.rb
+++ spec/integration/vm/cookbooks/apt-mirror/recipes/default.rb
@@ -1,4 +1,12 @@
-execute "sed -i -e 's| \\(http[^ ]\\+\\)| mirror://mirrors.ubuntu.com/mirrors.txt|g' /etc/apt/sources.list"
+require 'net/http'
+require 'uri'
+
+apt_servers = Net::HTTP.get(URI.parse('http://mirrors.ubuntu.com/mirrors.txt')).split("\n")
+# ftp.riken.jp is unstable and slow?
+apt_servers.delete('http://ftp.riken.jp/Linux/ubuntu/')
+apt_server = apt_servers[rand(apt_servers.size)]
+
+execute "sed -i -e 's| \\(http[^ ]\\+\\)| #{apt_server}|g' /etc/apt/sources.list"
 
 include_recipe 'apt'
 
