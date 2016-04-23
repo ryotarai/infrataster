f71e45ef2b3e5733baa50faa8d61cc3f18a30b67
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-04-23 15:14:18 +0900

    remove the one liner shell
    https://github.com/ryotarai/infrataster/pull/69#issuecomment-209197056

--- lib/infrataster/server.rb
+++ lib/infrataster/server.rb
@@ -155,15 +155,17 @@ module Infrataster
     def fetch_all_addresses
       result = []
       uname = ssh_exec('uname -s')
-      ssh_exec('UNAME=$( uname -s ); case ${UNAME} in Linux) PATH="/sbin:$PATH" ip addr ;; FreeBSD) /sbin/ifconfig ;; OpenBSD) /sbin/ifconfig ;; esac').each_line do |line|
-        case uname
-        when /(FreeBSD|OpenBSD)/
+      case uname
+      when /(FreeBSD|OpenBSD)/
+        ssh_exec('/sbin/ifconfig').each_line do |line|
           # inet 169.254.252.26 netmask 0xfffffffc broadcast 169.254.252.27 (regular interface)
           # inet 10.100.255.2 --> 10.100.255.1 netmask 0xffffffff (tunnel interface)
           if %r{inet (\S+) (?:-->[ ].+[ ])?netmask} =~ line
             result << $1
           end
-        else
+        end
+      else
+        ssh_exec('/sbin/ip addr').each_line do |line|
           # Linux
           # inet 10.0.2.15/24 brd 10.0.2.255 scope global eth0
           if %r{inet ([^/]+)} =~ line
